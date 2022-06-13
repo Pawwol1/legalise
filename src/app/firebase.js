06eae94806569f7a3473f4ba1a279f70c18ce793
Pawwol1 <wolski.pawel1@gmail.com>
2022-06-13 17:11:26 +0200

    firebase update

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyD58xzkikXXLe-R0xriDhjs2SDd4amf5WY",
     authDomain: "legalise-3574c.firebaseapp.com",
+    databaseURL: "https://legalise-3574c-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "legalise-3574c",
     storageBucket: "legalise-3574c.appspot.com",
     messagingSenderId: "1052402813104",
